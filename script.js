86e8902e808cd75189ec61e4a684a4eb50f4c586
Mark Anthony <manthonyamaro@yahoo.com>
2023-05-07 20:38:47 -0500

    Created base HTML with ID tags to pull from questions created in JavaScript that has an answer outcome that will later be used to place a value in the score

---
+++ script.js
@@ -0,0 +1,35 @@
+//Questions to be used for the quiz
+const quizData = [
+    {
+      question: "What does HTML stand for?",
+      choices: [
+        "Hyper Text Markup Language",
+        "Highly Textured Markup Language",
+        "Hyperlink Text Management Language",
+        "Home Tool Markup Language"
+      ],
+      answer: 0
+    },
+    {
+      question: "What does CSS stand for?",
+      choices: [
+        "Colorful Style Sheets",
+        "Cascading Style Sheets",
+        "Creative Style Sheets",
+        "Computer Style Sheets"
+      ],
+      answer: 1
+    },
+    {
+      question: "What does JS stand for?",
+      choices: [
+        "JavaSimple",
+        "JavaServer",
+        "JavaSyntax",
+        "JavaScript"
+      ],
+      answer: 3
+    }
+  ];
+
+  
