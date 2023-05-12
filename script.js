ae471d75f5cec3fa9b8f404753451550e579ed77
Mark Anthony <manthonyamaro@yahoo.com>
2023-05-11 19:21:01 -0500

    HTML updates-style display added to hide, span used for time with the ID tagged to it, game over paragraph closed with style being none to hide, score tagged by id within a span closed with a paragraph. Javascript updates-score,initial, and time added to variables that can not be changed and the use of let to set the numerical state of what it is attached to.showquestion increased code to resolve the loop when called and increments the score if correct answer is made and appended a button when moving on to another question after selecting an answer. set a timer fuction for every second with decrease and clears the time value when quiz is completed. enquiz function applied when function fires to clear the timer and display the score and gameover tags

--- script.js
+++ script.js
@@ -38,12 +38,61 @@ const quizData = [
     const questionEl = document.getElementById("question");
     const choicesEl = document.getElementById("choices");
     const submitBtn = document.getElementById("submit-button");
-
+    const gameOver = document.getElementById("game-over");
+    const timeEl = document.getElementById("time");
+    const initialsEl = document.getElementById("initials");
+    const scoreEl = document.getElementById("score");
+    let currentQuestion = 0
+    let score = 0;
+    let timeLeft = 60;
+    let timer;
 //start function
     function startQuiz() {
       startBtn.style.display = "none";
       quiz.style.display = "block";
+      showQuestion();
+      startTimer();
     }
+//display current question into the next
+function showQuestion() {
+  const currentQuiz = quizData[currentQuestion];
+  questionEl.textContent = currentQuiz.question;
+  choicesEl.innerHTML = "";
+  for (let i = 0; i < currentQuiz.choices.length; i++) {
+    const choice = currentQuiz.choices[i];
+    const btn = document.createElement("button");
+    btn.textContent = choice;
+    btn.addEventListener("click", function() {
+      if (i === currentQuiz.answer) {
+        score++;
+      }
+      if (currentQuestion < quizData.length - 1) {
+        currentQuestion++;
+        showQuestion();
+      } else {
+        endQuiz();
+      }
+    });
+    choicesEl.appendChild(btn); //appended button to allow choices to continue when answer selected
+  }
+}
+      function startTimer() {
+        timer = setInterval(function() {
+          timeLeft--;
+          timeEl.textContent = "Time: " + timeLeft;
+          if (timeLeft <= 0) {
+            clearInterval(timer);
+            endQuiz();
+          }
+        }, 1000);
+      }
+//end quiz function
+function endQuiz() {
+  clearInterval(timer);
+  quiz.style.display = "none";
+  gameOver.style.display = "block";
+  scoreEl.textContent = score;
+}
 
     //call the button
     startBtn.addEventListener("click", startQuiz);
